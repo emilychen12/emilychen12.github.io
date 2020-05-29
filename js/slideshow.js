825d095899cfd1bda957d1b590590d4adcfb2980
Emily Chen <ec745@cornell.edu>
2020-05-29 01:43:21 -0400

    added vcalumni app

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  //zenithia slides
   var images = [
     "images/zenithia_0.png", 
     "images/zenithia_1.png",
@@ -17,7 +18,7 @@ $(document).ready(function() {
     $("#zenithia_slides").attr("src", images[currindex]);
     // src: https://css-tricks.com/restart-css-animation/
     var el = $("#zenithia_slides"),  
-     newone = el.clone(true);        
+    newone = el.clone(true);        
     el.before(newone);    
     $("." + el.attr("class") + ":last").remove();
   });
@@ -33,7 +34,32 @@ $(document).ready(function() {
     // src: https://css-tricks.com/restart-css-animation/
     var el = $("#zenithia_slides"),  
     newone = el.clone(true);        
-   el.before(newone);    
-   $("." + el.attr("class") + ":last").remove();
+    el.before(newone);    
+    $("." + el.attr("class") + ":last").remove();
+  });
+
+
+  //vcalumni slides
+  var vcimages = [
+    "images/vcapp1.png", 
+    "images/vcapp2.png",
+    "images/vcapp3.png",
+  ];
+  var curr = 0;
+
+  $("#next2").click(function() {
+    curr += 1;
+    if(curr > 2) {
+      curr = 0;
+    }
+    $("#vcalumni_slides").attr("src", vcimages[curr]);
+  });
+
+  $("#back2").click(function() {
+    curr -= 1;
+    if(curr < 0) {
+      curr = 2;
+    } 
+    $("#vcalumni_slides").attr("src", vcimages[curr]);
   });
 });
